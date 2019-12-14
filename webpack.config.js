d74996c20556b5bae07ac93d0aa5e3777622adcb
chenbingchang(陈柄昌) <940001047@qq.com>
2019-12-14 17:37:29 +0800

    备份

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); // 生成html并且把
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // 打包前自动把output下面的目录清空
 const VueLoaderPlugin = require('vue-loader/lib/plugin'); // 它的职责是将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块。例如，如果你有一条匹配 /\.js$/ 的规则，那么它会应用到 .vue 文件里的 <script> 块。
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // 压缩js，mode改成production会内部自己调用Uglifyjs
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 把css抽取成单独的文件
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin; // 打包后每个文件包含的依赖，生产环境用
 
 /**
  * HtmlWebpackPlugin 插件，自动生成HTML并且自动引入打包好的js
@@ -16,14 +17,17 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 把css抽取
  * 要导入 CSV、TSV 和 XML，你可以使用 csv-loader 和 xml-loader。让我们处理这三类文件：
  */
 
+// 1、抽取公共代码，import自己写的公共代码；2、vendor抽取成单独文件
+
 module.exports = {
   // 入口文件，可以配置多个
   entry: './src/main.js',
   // 出口文件
   output: {
-    filename: '[name].[hash].js', // 文件名称
+    filename: '[name].js', // 文件名称
     path: path.resolve(__dirname, 'dist'), // 文件保存的路劲
-    publicPath: '/' // 最终体现在静态资源的uri中，而不是在打包后放到指定的目录中
+    publicPath: '/', // 最终体现在静态资源的uri中，而不是在打包后放到指定的目录中
+    chunkFilename: '[name].bundle.js' // 非入口(non-entry) chunk 文件的名称。例如动态引入的文件
   },
   // 开发工具
   devtool: 'inline-source-map', // 代码出错时可以查看到打包前的代码，方便调试
@@ -61,15 +65,6 @@ module.exports = {
         test: /\.scss$/,
         use: [
           'style-loader', // 开发环境，生产环境要用插件mini-css-extract-plugin抽成单独的文件
-          // {// 生产环境
-          //   loader: MiniCssExtractPlugin.loader,
-          //   options: {
-          //     // you can specify a publicPath here
-          //     // by default it uses publicPath in webpackOptions.output
-          //     // publicPath: '../',
-          //     // hmr: process.env.NODE_ENV === 'development'
-          //   }
-          // },
           {
             loader: 'css-loader',
             options: { importLoaders: 2 }
@@ -78,8 +73,8 @@ module.exports = {
           'sass-loader'
         ]
       },
-      // 解析图片
       /**
+       * 解析图片
        * file-loader/url-loader
        * 如果图片变成<img src="[object Module]"/>这种情况是因为版本问题，可以使用的："url-loader": "^2.1.0","file-loader": "^4.2.0"
        */
@@ -89,10 +84,9 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 1024 // 8KB
+              limit: 1024 // 1KB，小于1KB的图片将被转换成base64，否则就是正常的图片
             }
           }
-          // 'file-loader'
         ]
       },
       // 解析字体
@@ -113,25 +107,57 @@ module.exports = {
   },
   optimization: {
     // 压缩JS,这是最新的方式，以前一般是放到plugins配置里面
-    minimizer: [new UglifyJsPlugin()]
+    // minimizer: [new UglifyJsPlugin()]
+    /**
+     * 对vendor公共代码进行切割，会把用到的第三方库都放到一个文件里
+     */
+    splitChunks: {
+      // chunks: 'all', // 这表明将选择哪些块进行优化,有效值为all，async和initial
+      cacheGroups: {
+        // 初始化的第三方库方到一起
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'initial',// 分离初始化的可以了，按需加载的会自己生成一个文件
+          priority: 100
+        },
+        // 项目自己的公用代码则按原来的名称，各种打包成一个文件
+        common: {
+          test: /[\\/]common[\\/]/,// 公共代码的目录，根据实际修改
+          name: function(module, chunks, cacheGroupKey) {
+            /**
+             * path.sep:  路径分隔符。在linux上是/，在windows上是\。
+             * module.resource  文件的绝对路径
+             */
+            let lastSepIndex = module.resource.lastIndexOf(path.sep)// 最后路劲分割符最后的下标
+            let lastPointIndex = module.resource.lastIndexOf('.')// 文件后缀的下标
+            let name = module.resource.substr(lastSepIndex + 1, lastPointIndex)
+            
+            return name
+          },
+          chunks: 'async',// 如果a.js是按需加载，那么a.js里面通过import命令导入的js也是async类型
+          priority: 20,
+          minChunks: 2,
+          minSize: 30000// 小于30KB的则不用分离，否则网络请求性能更满
+        }
+      }
+    }
   },
   // 插件
   plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
-      // ignoreOrder: false // Enable to remove warnings about conflicting order
-    }),
     new VueLoaderPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html', // html的模板
       filename: 'index.html' // html文件的名称
-    })
+    }),
+    // new BundleAnalyzerPlugin()
   ],
+  // 解析
   resolve: {
-    alias: {}
+    // import或require时的别名
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 };
