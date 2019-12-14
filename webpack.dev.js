6b307d104505875e6ed645fa7b987cd95437e735
chenbingchang(陈柄昌) <940001047@qq.com>
2019-12-14 19:52:24 +0800

    本地开发配置清除无用代码

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,6 @@ const path = require('path'); // node path模块
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // 生成html并且把，js/css自动引入
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // 打包前自动把output下面的目录清空
 const VueLoaderPlugin = require('vue-loader/lib/plugin'); // 它的职责是将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块。例如，如果你有一条匹配 /\.js$/ 的规则，那么它会应用到 .vue 文件里的 <script> 块。
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // 压缩js，mode改成production会内部自己调用Uglifyjs
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin; // 打包后每个文件包含的依赖，生产环境用
 
 /**
  * HtmlWebpackPlugin 插件，自动生成HTML并且自动引入打包好的js
@@ -106,39 +103,40 @@ module.exports = {
     ]
   },
   optimization: {
-    // 压缩JS,这是最新的方式，以前一般是放到plugins配置里面
-    // minimizer: [new UglifyJsPlugin()]
     /**
      * 对vendor公共代码进行切割，会把用到的第三方库都放到一个文件里
      */
     splitChunks: {
       // chunks: 'all', // 这表明将选择哪些块进行优化,有效值为all，async和initial
       cacheGroups: {
-        // 初始化的第三方库方到一起
+        // 初始化的第三方库放到一个文件
         vendors: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
-          chunks: 'initial',// 分离初始化的可以了，按需加载的会自己生成一个文件
+          chunks: 'initial', // 分离初始化的可以了，按需加载的会自己生成一个文件
           priority: 100
         },
-        // 项目自己的公用代码则按原来的名称，各种打包成一个文件
+        // 项目自己的公用代码则按原来的名称，各自打包成一个文件
         common: {
-          test: /[\\/]common[\\/]/,// 公共代码的目录，根据实际修改
+          test: /[\\/]common[\\/]/, // 公共代码的目录，根据实际修改
           name: function(module, chunks, cacheGroupKey) {
             /**
              * path.sep:  路径分隔符。在linux上是/，在windows上是\。
              * module.resource  文件的绝对路径
              */
-            let lastSepIndex = module.resource.lastIndexOf(path.sep)// 最后路劲分割符最后的下标
-            let lastPointIndex = module.resource.lastIndexOf('.')// 文件后缀的下标
-            let name = module.resource.substr(lastSepIndex + 1, lastPointIndex)
-            
-            return name
+            let lastSepIndex = module.resource.lastIndexOf(path.sep); // 路劲最后分割符最后的下标
+            if (lastSepIndex === -1) {
+              lastSepIndex = 0;
+            }
+            let lastPointIndex = module.resource.lastIndexOf('.'); // 文件后缀的下标
+            let name = module.resource.substring(lastSepIndex + 1, lastPointIndex);
+
+            return name;
           },
-          chunks: 'async',// 如果a.js是按需加载，那么a.js里面通过import命令导入的js也是async类型
+          chunks: 'all', // 如果a.js是按需加载，那么a.js里面通过import命令导入的js也是async类型
           priority: 20,
           minChunks: 2,
-          minSize: 30000// 小于30KB的则不用分离，否则网络请求性能更满
+          minSize: 0 // 小于30KB的则不用分离，否则网络请求性能更满
         }
       }
     }
@@ -150,8 +148,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html', // html的模板
       filename: 'index.html' // html文件的名称
-    }),
-    // new BundleAnalyzerPlugin()
+    })
   ],
   // 解析
   resolve: {
